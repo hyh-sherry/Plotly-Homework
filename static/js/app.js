ddc98cb93b0be98f162debb62cb71e71b1910e7d
hyh-sherry <49596048+hyh-sherry@users.noreply.github.com>
2019-09-20 22:15:43 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
-// 1. Use the D3 library to read in `samples.json`.
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 d3.json("./samples.json").then(function(d) {
+    // Create a horizontal bar chart
     var sampleData = d.samples[0]
     var sampleValues = sampleData.sample_values;
     var otuIds = sampleData.otu_ids;
@@ -9,14 +8,15 @@ d3.json("./samples.json").then(function(d) {
 
     // slice top 10
     var topTenValues = sampleValues.slice(0,10);
-    otuIds.forEach(d => `"OTU ${d}`)
-    var topTenIds = otuIds.slice(0, 10);
+    var modifiedIds = otuIds.map(d => {return `OTU ${d}`});
+    var topTenIds = modifiedIds.slice(0, 10);
     var topTenLabels = otuLabels.slice(0, 10);
-    console.log(topTenIds);
+    //console.log(topTenIds);
 
     var trace1 = {
-        x: topTenValues,
-        y: topTenIds,
+        x: topTenValues.reverse(),
+        y: topTenIds.reverse(),
+        hovertext: topTenLabels.reverse(),
         type: "bar",
         orientation: "h"
     };
@@ -27,5 +27,48 @@ d3.json("./samples.json").then(function(d) {
         title: "Top 10 OTUs Found"
     };
 
-    Plotly.newPlot("bar", data, layout)
+    Plotly.newPlot("bar", data, layout);
+
+    // Create a bubble chart
+    var trace2 = {
+        x: otuIds,
+        y: sampleValues,
+        hovertext: otuLabels,
+        type: "scatter",
+        mode: "markers",
+        marker: {
+            size: sampleValues,
+            color: otuIds
+        }
+    };
+
+    var data2 = [trace2];
+
+    var layout2 = {
+        xaxis: {title:"OTU ID"}
+    };
+
+    Plotly.newPlot("bubble", data2, layout2);
+
+    // Display each key-value pair from the metadata JSON object somewhere on the page.   
+    var sampleMetadata = d.metadata[0];
+    //console.log(sampleMetadata)
+    var selection = d3.select("#sample-metadata").selectAll("div").data(sampleMetadata);
+
+    selection.enter()
+        .append("ul")
+        .merge(selection)
+        .classed("panel-body", true)
+        .html(function(data) {
+            return `<li>id: ${data.id}</li>
+                    <li>ethnicity: ${data.ethnicity}</li>
+                    <li>gender: ${data.gender}</li>
+                    <li>age: ${data.age}</li>
+                    <li>location: ${data.location}</li>
+                    <li>bbtype: ${data.bbtype}</li>
+                    <li>wfreq: ${data.wfreq}</li>`
+        });
+
+    selection.exit().remove(); 
+
 })
